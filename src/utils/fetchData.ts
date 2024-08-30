f2fe2718ff61b6e486737e6a911f28a5bd0c1829
SnhAenIgseAl <1789561096@qq.com>
2024-08-30 15:44:59 +0800

    fetch设置credentials为include

--- src/utils/fetchData.ts
+++ src/utils/fetchData.ts
@@ -6,6 +6,7 @@ export const fetchData = async (api: string, opition: RequestInit | undefined, c
     try {
         api = api.replace('/api', 'https://pilipili-server.icu')
         opition!.mode = 'cors'
+        opition!.credentials = 'include'
 
         let res = await fetch(api, opition)
         let data: BiliResType = await res.json()
